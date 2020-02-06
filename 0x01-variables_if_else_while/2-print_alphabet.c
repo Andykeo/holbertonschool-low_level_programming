a398b070524b095d532e572c7ecd2a23ef1b43f4
Andykeo <andikeo@hotmail.com>
2020-02-06 20:21:53 +0000

    print the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - use for
+ * Description: Prints a char
+ * Return: 0
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
