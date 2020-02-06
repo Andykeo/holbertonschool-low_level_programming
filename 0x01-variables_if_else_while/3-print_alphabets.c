7085c5e4d551db7d9f0ea2269ec4722be3b918b7
Andykeo <andikeo@hotmail.com>
2020-02-06 20:32:44 +0000

    print the alphabets in minus and mayus

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - use for
+ * Description: Prints a char
+ * Return: 0
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		putchar(az);
+	}
+	for (az = 'A'; az <= 'Z'; az++)
+	{
+		putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
