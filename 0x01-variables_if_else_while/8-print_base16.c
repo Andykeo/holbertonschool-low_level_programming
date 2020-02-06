34dd79300fbaf32a3a27336df7839a6a12ba7412
Andykeo <andikeo@hotmail.com>
2020-02-06 21:35:11 +0000

    print hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - use for
+ * Description: Prints a char
+ * Return: 0
+ */
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num == 9)
+		{
+			num = 'a';
+		}
+	}
+	for (num = 'a'; num <= 'f'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
