77cdb50b939a871376c292126441a39931f7c85b
Andykeo <andikeo@hotmail.com>
2020-02-06 20:41:18 +0000

    print the aphabet whitout e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - use for
+ * Description: Prints a char
+ * Return: 0
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		if(az != 'e' && az != 'q')
+		{
+			putchar(az);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
